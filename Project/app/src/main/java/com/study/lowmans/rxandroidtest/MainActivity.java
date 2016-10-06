8a0b5b0b1b311ee33d32c2498b7efa5892b70f1a
Changsik <lowmans00@gmail.com>
2016-10-06 09:48:50 +0900

    retrolambda 추가 및 기능확인

--- Project/app/src/main/java/com/study/lowmans/rxandroidtest/MainActivity.java
+++ Project/app/src/main/java/com/study/lowmans/rxandroidtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.study.lowmans.rxandroidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button1).setOnClickListener(v -> {
+            Log.i("#@#", "Hello World");
+        });
+
     }
 }
